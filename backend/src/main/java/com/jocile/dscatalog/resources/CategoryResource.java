46f855951b9c27e97f96173f26b52004a14cbce1
Jocilé Serra <jocile@gmail.com>
2023-07-12 19:44:23 -0300

    ✨ controller: Category resource

---
+++ backend/src/main/java/com/jocile/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.jocile.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jocile.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+  @GetMapping
+  public ResponseEntity<List<Category>> findAll() {
+    List<Category> list = new ArrayList<>();
+    list.add(new Category(1L, "Books"));
+    list.add(new Category(2L, "Electronics"));
+    return ResponseEntity.ok().body(list);
+  }
+}
