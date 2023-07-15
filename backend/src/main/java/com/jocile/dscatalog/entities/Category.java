f7bb91070e48bde0e391fa8ea250e456a616a77e
Jocilé Serra <jocile@gmail.com>
2023-07-15 12:33:13 -0300

    🔧 model: Category entity JPA config

--- backend/src/main/java/com/jocile/dscatalog/entities/Category.java
+++ backend/src/main/java/com/jocile/dscatalog/entities/Category.java
@@ -2,9 +2,19 @@ package com.jocile.dscatalog.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 
