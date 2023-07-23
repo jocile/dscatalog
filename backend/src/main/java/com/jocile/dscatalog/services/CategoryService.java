bc0101ca567d0ccb710be4ade379fffcd689bc90
Jocilé Serra <jocile@gmail.com>
2023-07-23 19:16:57 -0300

    ✨ Control: findById endpoint

--- backend/src/main/java/com/jocile/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jocile/dscatalog/services/CategoryService.java
@@ -1,7 +1,7 @@
 package com.jocile.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +22,12 @@ public class CategoryService {
   public List<CategoryDTO> findAll() {
     List<Category> list = repository.findAll();
     return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+  }
+
+  @Transactional(readOnly = true)
+  public CategoryDTO findById(Long id) {
+    Optional<Category> obj = repository.findById(id);
+    Category entity = obj.get();
+    return new CategoryDTO(entity);
   }
 }
