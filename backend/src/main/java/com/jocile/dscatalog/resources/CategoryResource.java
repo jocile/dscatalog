b382afbd3f983d8e686bb883022a6f22e5c5651c
Jocilé Serra <jocile@gmail.com>
2023-07-23 21:36:45 -0300

    ✨ Control: Post insert Category

--- backend/src/main/java/com/jocile/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/jocile/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.jocile.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jocile.dscatalog.dto.CategoryDTO;
 import com.jocile.dscatalog.services.CategoryService;
@@ -29,5 +33,13 @@ public class CategoryResource {
   public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
     CategoryDTO dto = service.findById(id);
     return ResponseEntity.ok().body(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+    dto = service.insert(dto);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(dto.getId()).toUri();
+    return ResponseEntity.created(uri).body(dto);
   }
 }
